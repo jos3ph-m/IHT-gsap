b1e0275ddf4d1d2a8a52c9d5a1f9ad5bfa5d3d51
Joseph M <jmedina1985@hotmail.com>
2020-10-07 23:41:53 -0700

    cleaned up leave

--- src/js/app.js
+++ src/js/app.js
@@ -19,15 +19,14 @@ const animationLeave = (container) => {
   });
 };
 
+// we need to wait for the animation to finish before enter animation plays
 barba.init({
   transitions: [
     {
       once({ next }) {
         animationEnter(next.container);
       },
-      leave({ current }) {
-        animationLeave(current.container);
-      },
+      leave: ({ current }) => animationLeave(current.container),
       enter({ next }) {
         animationEnter(next.container);
       },
