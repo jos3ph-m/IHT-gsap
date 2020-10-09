db58122e34723fca81ea61698e92f6b374e27ad4
Joseph M <jmedina1985@hotmail.com>
2020-10-08 23:37:12 -0700

    animationEnter timeline

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,27 @@
 import barba from '@barba/core';
+import gsap from 'gsap/gsap-core';
 import { animationEnter, animationLeave } from './animations';
+
+const resetActiveLink = () => {
+  gsap.set('a.is-active span', {
+    xPercent: -100,
+    transformOrigin: 'left',
+  });
+};
 
 // we need to wait for the animation to finish before enter animation plays
 barba.init({
   transitions: [
     {
       once({ next }) {
-        animationEnter(next.container);
+        resetActiveLink();
+        gsap.from('header a', {
+          duration: 0.6,
+          yPercenmt: 100,
+          stagger: 0.2,
+          ease: 'power1.out',
+          onComplete: () => animationEnter(next.container),
+        });
       },
       leave: ({ current }) => animationLeave(current.container),
       enter({ next }) {
