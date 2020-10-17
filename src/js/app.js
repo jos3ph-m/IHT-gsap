70afd98b24b886e5a2b72f22a2b736e12c870b79
Joseph M <jmedina1985@hotmail.com>
2020-10-16 22:35:08 -0700

    revert stagger

--- src/js/app.js
+++ src/js/app.js
@@ -24,7 +24,7 @@ barba.init({
         gsap.from('header a', {
           duration: 0.6,
           yPercent: 100,
-          stagger: 0.3,
+          stagger: 0.2,
           ease: 'power1.out',
           onComplete: () => animationEnter(next.container),
         });
